2bb2a07a1a877a5598e69e41c881f2e8a7ef4e95
gergelytakacs <gergelytakacs@gergelytakacs.com>
2018-04-20 16:27:29 +0200

    quadratic 3D minimization example

---
+++ kap04/kvadraticka3Dmin.m
@@ -0,0 +1,18 @@
+clc; clear;
+
+H=[2,-1;                       % Konstanty
+   1, 2];   
+F=[1  2];                      % Konstanty
+u = sym('u', [2 1]);           % Symbolicky vektor
+J(u)=u.'*H*u+F*u;              % Definica funkcie
+J=simplify(J)                  % Zjednodusenie
+nablaJ=gradient(J,u)           % Gradient
+[u1ast u2ast]=solve(nablaJ==0) % Vyries pre u
+
+[u1,u2] = meshgrid(-2:0.25:1,-2:0.25:1);
+surf(u1,u2,double(J(u1,u2)));   % Vykreslenie
+hold on                         % Podrzanie grafu
+plot3(u1ast,u2ast,J(u1ast,u2ast),'o','MarkerSize',10) 
+xlabel('u_1 (-)')               % Znacenie osi x
+ylabel('u_2 (-)')               % Znacenie osi y
+zlabel('J(u) (-)')              % Znacenie osi z
