9dbb0346655208f98d3e38b48fbf952659cc104e
gergelytakacs <gergelytakacs@gergelytakacs.com>
2018-05-29 20:11:01 +0200

    J was incorrect
    The last term in the transformed way must be included

--- kap05/testvykreslitJ.m
+++ kap05/testvykreslitJ.m
@@ -1,19 +1,19 @@
 clc; clear all;
 
-Phi=[0 1; 0 0]; Gamma=[0 1]';         % Dynamika                        
-Q=eye(2); R=1; x=[1 1]'; Ts=1; np=5;  % Vahy, parametre
+Phi=[0 1; 0 0]; Gamma=[0 1]';          % Dynamika                        
+Q=eye(2); R=1; x=[1 1]'; Ts=1; np=5;   % Vahy, parametre
 [A,B,C,D] = c2dm(Phi,Gamma,[1 1],0,Ts);% Diskretizacia
 
 % Offline cast
 [K,P]=iterdlqr(A,B,Q,R,100);          % Koncove vah.
-[H,G]=ucelovafunkcia(A,B,np,Q,R,P);   % Ucelova f.
+[H,G,F]=ucelovafunkcia(A,B,np,Q,R,P); % Ucelova f.
 
 % Online cast
 for i=1:10                            % Trvanie sim.
  upr=-inv(H)*G*x;                     % Predikcia u
  u=upr(1);                            % Prvy clen
+ vykreslitJ(x,upr,H,G,F)              % Priebeh j
  x=A*x+B*u;                           % Aplikacia u
- vykreslitJ(x,upr,H,G)                % Priebeh j
 end
 
 
--- kap05/vykreslitJ.m
+++ kap05/vykreslitJ.m
@@ -1,10 +1,10 @@
-function vykreslitJ(x,upr,H,G)% Funkcia na vykreslenie J
+function vykreslitJ(x,upr,H,G,F)      % Vykreslenie J
 
  persistent Jk                        % Trvale premenne
  if isempty(Jk)                       % Ak este neexist.
   Jk=[];                              % Inicializuj
  end                                  % Koniec podm.
- j=upr'*H*upr+2*x'*G'*upr;            % Ucelova funkcia
+ j=upr'*H*upr+2*x'*G'*upr+x'*F*x;     % Ucelova funkcia
  Jk=[Jk j];                           % Skladovanie Jk  
          
  plot(Jk,'LineWidth',1,'Marker','.')  % Stavy
